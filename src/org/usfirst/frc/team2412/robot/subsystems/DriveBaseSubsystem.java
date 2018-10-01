118c4039978579d0c29847b1deb9a5f7070dd9e8
s-engt <s-engt@52S50013777.it.bsd405.org>
2018-10-01 00:25:12 -0700

    Run DriveCommand by default.

--- src/org/usfirst/frc/team2412/robot/subsystems/DriveBaseSubsystem.java
+++ src/org/usfirst/frc/team2412/robot/subsystems/DriveBaseSubsystem.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
+import org.usfirst.frc.team2412.robot.commands.DriveCommand;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveBaseSubsystem extends Subsystem {
 	@Override
 	protected void initDefaultCommand() {
+		setDefaultCommand(new DriveCommand());
 	}
 	
 	public void drive(Joystick stick) {
