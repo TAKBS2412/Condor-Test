d689d9666ad926b0c3a1790c4c9caf49919b30b1
s-engt <s-engt@52S50013777.it.bsd405.org>
2018-10-01 23:54:33 -0700

    Added roller instance variable to IntakeInOutSubsystem.

--- src/org/usfirst/frc/team2412/robot/subsystems/IntakeInOutSubsystem.java
+++ src/org/usfirst/frc/team2412/robot/subsystems/IntakeInOutSubsystem.java
@@ -1,21 +1,28 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
+import org.usfirst.frc.team2412.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class IntakeInOutSubsystem extends Subsystem {
+	
+	private WPI_TalonSRX roller = RobotMap.roller;
+	
 	@Override
 	protected void initDefaultCommand() {
 	}
 	
 	public void intakeIn() {
-		
+		roller.set(1.0);
 	}
 	
 	public void intakeOut() {
-		
+		roller.set(-1.0);
 	}
 	
 	public void intakeStop() {
-		
+		roller.set(0.0);
 	}
 }
